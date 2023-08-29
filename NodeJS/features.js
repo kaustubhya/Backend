7d8df13a975687d2dbb12e9970fcf6a31772d212
kaustubhya <kaustubhyaksd@gmail.com>
2023-08-30 03:59:39 +0530

    here

---
+++ NodeJS/features.js
@@ -0,0 +1,10 @@
+const gfName = "MrsRando";
+const gfName2 = "MrsRando2";
+const gfName3 = "MrsRando3";
+
+
+export default gfName;
+
+// expoting and importing multiple files
+export {gfName2, gfName3};
+
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,12 +1,51 @@
-console.log("Hi");
+// console.log("Hi");
 
-console.log(200/4);
+// console.log(200/4);
 
 
-console.log("Hi there");
+// console.log("Hi there");
 
-console.log("Hey there");
+// console.log("Hey there");
 
-console.log("Hey there");
+// console.log("Hey there");
+
+
 
 // making servers in backend
+
+// const http = require("http");
+
+// // importing file based module
+// const gfName = require("./features");
+import http from "http";
+import gfName from "./features.js";
+import { gfName2 } from "./features.js";
+
+console.log(gfName);
+const server = http.createServer((req,res) => {
+    // console.log(req.url);
+    // res.end("<h1>Noice</h1>")
+
+    // Routing
+    if(req.url==="/about") {
+        res.end("<h1>About Page</h1>");
+    }
+
+    else if(req.url==="/") {
+        res.end("<h1>Home Page</h1>");
+    }
+
+    else if(req.url==="/contact") {
+        res.end("<h1>Contact Page</h1>");
+    }
+    else {
+        res.end("<h1>Error 404: Page Not Found</h1>");
+
+    }
+});
+
+// listening on server - getting server output
+
+server.listen(5000, () => {
+    console.log("Server is Working Fine");
+});
